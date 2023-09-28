27025effe3805087111fcbcb2af20ec2b8e6b95d
AffanShaikh100 <133389464+AffanShaikh100@users.noreply.github.com>
2023-09-28 16:39:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
+
 function createTable() {
     //Write your code here
-  
+  let table = document.getElementById("myTable");
+  let row= prompt("Input number of rows");
+  let column = prompt("Input number of columns");
+
+  table.innerHTML = ""; 
+	for(let i=0; i<row; i++)
+	{
+      let rowcell=document.createElement("tr");
+	     for(let j=0; j<column; j++)
+           {
+			let cell = document.createElement("td");
+		    
+		    rowcell.appendChild(cell);   
+			cell.textContent="Row-i Column-j";
+		    }
+		table.appendChild(rowcell);
+	}
+	
 }
